9d10b3910d047db62c4aea029ddc290c29f9f4c6
Ruchi Jain <ruchijain@fastmail.com>
2019-04-16 20:33:47 -0400

    Correct indentation in solution files lol

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -11,52 +11,52 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = {
-        if (c == 0 || c == r) {
-            1
-        } else {
-            pascal(c-1, r-1) + pascal(c, r-1)
-        }
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) {
+      1
+    } else {
+      pascal(c-1, r-1) + pascal(c, r-1)
     }
-
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = {
-        def balanceAux(openParens: List[Char], chars: List[Char]): Boolean = {
-            if (chars.isEmpty) {
-                openParens.isEmpty
-            } else if (chars.head == '(') {
-                balanceAux('(' :: openParens, chars.tail)
-            } else if (chars.head == ')') {
-                if (openParens.isEmpty || openParens.head != '(') {
-                    false
-                } else {
-                    balanceAux(openParens.tail, chars.tail)
-                }
-            } else {
-                balanceAux(openParens, chars.tail)
-            }
-        }
-
-        balanceAux(Nil, chars)
-    }
-
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = {
-        if (money == 0 || coins.isEmpty) {
-            0
-        } else if (money.toFloat / coins.head == 1) {
-            1 + countChange(money, coins.tail)
-        } else if (money.toFloat / coins.head < 1) {
-            countChange(money, coins.tail)
-        } else {
-            countChange(money - coins.head, coins) + countChange(money, coins.tail)
-        }
-    }
-
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def balanceAux(openParens: List[Char], chars: List[Char]): Boolean = {
+      if (chars.isEmpty) {
+        openParens.isEmpty
+      } else if (chars.head == '(') {
+        balanceAux('(' :: openParens, chars.tail)
+      } else if (chars.head == ')') {
+        if (openParens.isEmpty || openParens.head != '(') {
+          false
+        } else {
+          balanceAux(openParens.tail, chars.tail)
+        }
+      } else {
+        balanceAux(openParens, chars.tail)
+      }
+    }
+
+    balanceAux(Nil, chars)
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0 || coins.isEmpty) {
+      0
+    } else if (money.toFloat / coins.head == 1) {
+      1 + countChange(money, coins.tail)
+    } else if (money.toFloat / coins.head < 1) {
+      countChange(money, coins.tail)
+    } else {
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
+  }
+
+}
